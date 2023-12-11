a3998b65ecc167aa7d328d87fac63278e5b63ea8
Rachel <rachel.uiux@gmail.com>
2023-12-11 18:49:05 +0900

    Revert "feature/ide-14-ci-setup test"

--- src/pages/GuestEnterPage.tsx
+++ src/pages/GuestEnterPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const GuestEnterPage = () => {
   return <div>GuestEnterPage</div>;
 };
--- src/pages/IDEPage.tsx
+++ src/pages/IDEPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const IDEPage = () => {
   return <div>IDEPage</div>;
 };
